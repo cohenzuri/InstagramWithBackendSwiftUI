8b6aec9871d7c2fac827179d3daa85cd69b7e3f0
cohenzuri <57574358+cohenzuri@users.noreply.github.com>
2023-01-08 17:51:46 +0200

    Upload post view UI

--- InstagramWithBackend/Features/UploadPost/Views/UploadPostView.swift
+++ InstagramWithBackend/Features/UploadPost/Views/UploadPostView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("Upload Post")
+    
+        VStack {
+            
+            if postImage != nil {
+                
+                Button {
+
+                  print("did tap upload post")
+
+                } label: {
+                    
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.white)
+                }
+                
+            } else {
+                
+                VStack(spacing: 70) {
+                    
+                    HStack(alignment: .top) {
+                        
+                        Image("post_1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 96, height: 96)
+                            .clipped()
+                        
+                        TextField("Enter your caption..",
+                                  text: $captionText)
+                    }
+                    
+                    Button {
+                        print("did tap post")
+                    } label: {
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(height: 50)
+                            
+                            Text("Share").font(.system(size: 16, weight: .semibold ))
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
     }
 }
 
