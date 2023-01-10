2d9d263634904a41981602eae73e91ecea3b9e68
cohenzuri <57574358+cohenzuri@users.noreply.github.com>
2023-01-10 21:10:38 +0200

    Add the authentication views Add firebase backend dependency

---
+++ InstagramWithBackend/Features/Authentication/Views/LoginView.swift
@@ -0,0 +1,38 @@
+//
+//  LoginView.swift
+//  InstagramWithBackend
+//
+//  Created by zuri cohen on 1/9/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        
+        ZStack {
+            
+            LinearGradient(colors: [.purple, .blue], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+            
+            Text("temp")
+            
+//            VStack {
+//                Image(uiImage: "")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 220,height: 100)
+//                    .foregroundColor(.white)
+//            }
+            
+        }
+       
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
