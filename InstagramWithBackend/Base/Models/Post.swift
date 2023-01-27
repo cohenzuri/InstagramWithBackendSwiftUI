05837f66444ff0d3dc1c035fa500d3e5e36e29a6
cohenzuri <57574358+cohenzuri@users.noreply.github.com>
2023-01-27 23:23:21 +0200

    Post api model

---
+++ InstagramWithBackend/Base/Models/Post.swift
@@ -0,0 +1,20 @@
+//
+//  Post.swift
+//  InstagramWithBackend
+//
+//  Created by zuri cohen on 1/27/23.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Post: Decodable, Identifiable {
+    @DocumentID var id: String?
+    let ownerUid: String
+    var ownerUsername: String
+    let caption: String
+    let likes: Int
+    let imageUrl: String
+    let timestamp: Timestamp
+    let ownerImageUrl: String
+}
